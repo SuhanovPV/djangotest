c64b06419dfdcd2ea09cb6e434fa0c6c194119b9
Sukhar <ssuxxarr@gmail.com>
2021-12-10 23:17:17 +0300

    added template for bb. Due to this modified views

--- bboard/views.py
+++ bboard/views.py
@@ -1,11 +1,8 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 from .models import Bb
 
 
 def index(request):
-    s = "Объявления 🦄\r\n\r\n"
-    for bb in Bb.objects.order_by('-published'):
-        s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
-
-    return HttpResponse(s + "🐉", content_type='text/plain; charset=utf-8')
+    bbs = Bb.objects.order_by('-published')
+    return render(request, 'bboard/index.html', {"bbs": bbs})
