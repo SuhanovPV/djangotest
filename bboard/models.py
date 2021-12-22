f0aa6397105540e0e937f2a65e28594d2687b263
Sukhar <ssuxxarr@gmail.com>
2021-12-22 20:34:43 +0300

    minor changes in models.py

--- bboard/models.py
+++ bboard/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core import validators
+from django.core.exceptions import ValidationError
 
 
 class Rubric(models.Model):
@@ -14,11 +16,28 @@ class Rubric(models.Model):
 
 
 class Bb(models.Model):
-    title = models.CharField(max_length=50, verbose_name="Товар")
+    title = models.CharField(max_length=50, verbose_name="Товар",
+                             validators=[validators.RegexValidator(regex='^.{3,}$')],
+                             error_messages={'invalid': 'Название товара должно содержать минимум 3 символа'})
     content = models.TextField(null=True, blank=True, verbose_name="Описание")
     price = models.FloatField(null=True, blank=True, verbose_name="Цена")
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name="Опубликовано")
     rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Рубрика')
+
+    def title_and_price(self):
+        if self.price:
+            return '%s (%.2f)' % (self.title, self.price)
+        else:
+            return self.title
+
+    def clean(self):
+        errors = {}
+        if not self.content:
+            errors['content'] = ValidationError('Укажите описание продаваемого товара')
+        if self.price and self.price < 0:
+            errors['price'] = ValidationError('Цена не может быть отрицательной')
+        if errors:
+            raise ValidationError(errors)
 
     class Meta:
         verbose_name_plural = "Объявления"
