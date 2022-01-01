09fad22f15408cac9eb324e3e8f27c4fcac26f7f
Sukhar <ssuxxarr@gmail.com>
2022-01-01 13:39:32 +0300

    Retern to class BBcreateView

--- bboard/urls.py
+++ bboard/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import BbCreateView, BbByRubricView, BbView, BbDetailView, BbAddView
 
 urlpatterns = [
-    path("detail/<int:pk>/",BbDetailView.as_view(), name='detail'),
+    path("detail/<int:pk>/", BbCreateView.as_view(), name='detail'),
     path("add/", BbAddView.as_view(), name='add'),
     path("<int:rubric_id>/", BbByRubricView.as_view(), name='by_rubric'),
     path("", BbView.as_view(), name='index'),
