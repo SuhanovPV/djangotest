ee78a1017d76df83328b1a0adafb6a191098e24d
Sukhar <ssuxxarr@gmail.com>
2022-01-03 15:55:43 +0300

    Add view Bb by Date(year, month, day)

--- bboard/urls.py
+++ bboard/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 
-from .views import BbCreateView, BbByRubricView, BbView, BbDetailView, BbAddView, BbUpdateView, BbDeleteView
+from .views import BbCreateView, BbByRubricView, BbView, BbDetailView, BbAddView, BbUpdateView, BbDeleteView, \
+    BbIndexView, BbByYear, BbByMonth, BbByDay
 
 urlpatterns = [
+    path("<int:year>/<int:month>/<int:day>", BbByDay.as_view(), name='by_day'),
+    path("<int:year>/<int:month>", BbByMonth.as_view(), name='by_month'),
+    path("<int:year>/<int:month>", BbByMonth.as_view(), name='by_month'),
+    path("<int:year>/", BbByYear.as_view(), name='by_year'),
     path("delete/<int:pk>/", BbDeleteView.as_view(), name='delete'),
     path("detail/<int:pk>/", BbDetailView.as_view(), name='detail'),
     path("update/<int:pk>/", BbUpdateView.as_view(), name='update'),
     path("add/", BbAddView.as_view(), name='add'),
-    path("<int:rubric_id>/", BbByRubricView.as_view(), name='by_rubric'),
-    path("", BbView.as_view(), name='index'),
+    path("rubric/<int:rubric_id>/", BbByRubricView.as_view(), name='by_rubric'),
+    path("", BbIndexView.as_view(), name='index'),
 ]
--- bboard/views.py
+++ bboard/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.views.generic.dates import ArchiveIndexView, YearArchiveView, MonthArchiveView, DayArchiveView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, FormView, UpdateView, DeleteView
 from django.views.generic.list import ListView
@@ -17,6 +18,63 @@ class BbView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
         context['bbs'] = Bb.objects.all()
+        context['rubrics'] = Rubric.objects.annotate(Count('bb'))
+        return context
+
+
+class BbIndexView(ArchiveIndexView):
+    model = Bb
+    date_field = 'published'
+    date_list_period = 'year'
+    template_name = 'bboard/index_by_archive.html'
+    context_object_name = 'bbs'
+    allow_empty = True
+
+    def get_context_data(self, *args, object_list=None, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['rubrics'] = Rubric.objects.annotate(Count('bb'))
+        return context
+
+
+class BbByYear(YearArchiveView):
+    model = Bb
+    date_field = 'published'
+    template_name = 'bboard/by_year.html'
+    context_object_name = 'bbs'
+    make_object_list = True
+    allow_empty = True
+
+    def get_context_data(self, *args, object_list=None, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['rubrics'] = Rubric.objects.annotate(Count('bb'))
+        return context
+
+
+class BbByMonth(MonthArchiveView):
+    model = Bb
+    date_field = 'published'
+    template_name = 'bboard/by_month.html'
+    context_object_name = 'bbs'
+    allow_empty = True
+    month_format = '%m'
+
+    def get_context_data(self, *args, object_list=None, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['rubrics'] = Rubric.objects.annotate(Count('bb'))
+        return context
+
+
+class BbByDay(DayArchiveView):
+    model = Bb
+    date_field = 'published'
+    template_name = 'bboard/by_day.html'
+    context_object_name = 'bbs'
+    allow_empty = True
+    month_format = '%m'
+    day_format = '%d'
+
+    def get_context_data(self, *args, object_list=None, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
         context['rubrics'] = Rubric.objects.annotate(Count('bb'))
         return context
 
@@ -99,6 +157,7 @@ class BbDeleteView(DeleteView):
         context = super().get_context_data(*args, **kwargs)
         context['rubrics'] = Rubric.objects.annotate(Count('bb'))
         return context
+
 
 def index(request):
     bbs = Bb.objects.all()
